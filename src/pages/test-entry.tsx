d47ac49ebfa3943334262bdedabfdfa15f6bd71b
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2020-01-26 15:28:31 +0100

    Add data fetch from firebase

--- src/pages/test-entry.tsx
+++ src/pages/test-entry.tsx
@@ -1,13 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import { FirebaseContext } from 'gatsby-plugin-firebase';
 import { getToken } from '../services/auth';
 import 'firebase/firestore';
+import { WithFirebase } from '../hocs/withFirebase';
+
+interface Training {
+  date: Date;
+  headcoach: string;
+  coaches: string[];
+}
 
 const TestEntry = () => {
   const [entry, setEntry] = useState('');
   const [hasError, setError] = useState(false);
+  const [trainings, setTrainings] = useState<Training[]>([]);
   const firebase = React.useContext(FirebaseContext);
+
+  useEffect(() => {
+    if (!firebase) return undefined;
+    const db = firebase.firestore();
+    let trainingsList: Training[] = [];
+    db.collection('test-trainings')
+      .get()
+      .then((allTrainings) => {
+        allTrainings.forEach((training) =>
+          trainingsList.push(training.data() as Training)
+        );
+        setTrainings(trainingsList);
+      });
+  }, [firebase]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEntry(event.target.value);
@@ -29,7 +51,6 @@ const TestEntry = () => {
   };
 
   const handleAddTraining = async () => {
-    const authToken = getToken();
     const db = firebase.firestore();
     try {
       await db.collection('test-trainings').add({
@@ -42,12 +63,22 @@ const TestEntry = () => {
     }
   };
 
+  const renderTrainings = () => {
+    return trainings.map((training) => (
+      <li>
+        {training.date.toString()}, {training.headcoach}
+      </li>
+    ));
+  };
+
   return (
     <Layout>
       {hasError && <h2>Submission failed </h2>}
       <input type="text" value={entry} onChange={handleChange} />
       <button onClick={handleSubmit}>Submit entry</button>
       <button onClick={handleAddTraining}>Add training</button>
+      <h3>Registered trainings</h3>
+      <ul>{renderTrainings()}</ul>
     </Layout>
   );
 };
