014d3121a52c4232eca52dcb9570db1357267d79
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2020-01-26 20:03:48 +0100

    Refactor with gatsby-theme-firebase

--- src/pages/test-entry.tsx
+++ src/pages/test-entry.tsx
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/layout';
-import { FirebaseContext } from 'gatsby-plugin-firebase';
-import { getToken } from '../services/auth';
-import 'firebase/firestore';
-import { WithFirebase } from '../hocs/withFirebase';
-import { firestore } from 'firebase';
+import { firestore, useFirestoreQuery } from 'gatsby-theme-firebase';
 
 interface Training {
-  date: firestore.Timestamp;
+  _id: string;
+  date: firebase.firestore.Timestamp;
   headcoach: string;
   coaches: string[];
 }
@@ -15,47 +12,22 @@ interface Training {
 const TestEntry = () => {
   const [entry, setEntry] = useState('');
   const [hasError, setError] = useState(false);
-  const [trainings, setTrainings] = useState<Training[]>([]);
-  const firebase = React.useContext(FirebaseContext);
 
-  useEffect(() => {
-    if (!firebase) return undefined;
-    const db = firebase.firestore();
-    let trainingsList: Training[] = [];
-    db.collection('test-trainings')
-      .get()
-      .then((allTrainings) => {
-        allTrainings.forEach((training) =>
-          trainingsList.push(training.data() as Training)
-        );
-        setTrainings(trainingsList);
-      });
-  }, [firebase]);
+  const [trainings, isLoading, error] = useFirestoreQuery<Training>(
+    firestore.collection('test-trainings').orderBy('date')
+  );
 
+  console.log(trainings);
+  console.log(error);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEntry(event.target.value);
   };
 
-  const handleSubmit = async () => {
-    console.log('entry is: ', entry);
-    const authToken = getToken();
-    console.log(authToken);
-    // if (!authToken) return Error('No token in session');
-    const db = firebase.firestore();
-    try {
-      await db.collection('test').add({
-        value: entry,
-      });
-    } catch (err) {
-      setError(true);
-    }
-  };
-
   const handleAddTraining = async () => {
-    const db = firebase.firestore();
+    const db = firestore;
     try {
       await db.collection('test-trainings').add({
-        date: new Date(2020, 2, 3),
+        date: new Date(2021, 2, 3),
         headcoach: 'test-Maria',
         coaches: ['test-Gustav', 'test-Jens'],
       });
@@ -66,7 +38,7 @@ const TestEntry = () => {
 
   const renderTrainings = () => {
     return trainings.map((training) => (
-      <li>
+      <li key={training._id}>
         {training.date.toDate().toLocaleDateString()}, {training.headcoach}
       </li>
     ));
@@ -76,10 +48,10 @@ const TestEntry = () => {
     <Layout>
       {hasError && <h2>Submission failed </h2>}
       <input type="text" value={entry} onChange={handleChange} />
-      <button onClick={handleSubmit}>Submit entry</button>
       <button onClick={handleAddTraining}>Add training</button>
+      {/* <button onClick={handleSubmit}>Submit entry</button> */}
       <h3>Registered trainings</h3>
-      <ul>{renderTrainings()}</ul>
+      {isLoading ? <p>Loading</p> : <ul>{renderTrainings()}</ul>}
     </Layout>
   );
 };
